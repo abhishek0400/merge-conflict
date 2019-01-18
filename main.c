7aa5a16c23e62a519294630ee5a1a0ba03bd86f3
devops-intellipaat <hemant@intellipaat.com>
2019-01-18 02:33:59 +0000

    added feature2

--- main.c
+++ main.c
@@ -2,14 +2,21 @@ pseudo code
 {
 Initial Function()
 {
-  //Sample Code
+ 
 }
+
 
 Initial Security()
 {
  //Sample Code
 }
 
+
+feaature2()
+{
+  //Sample Code for Feature 2
+}
+>>>>>>> ec34fc6 (added feature2)
 
 
 }
